6302f87b131cfbcf71b66db4c5d6d80c8fd5e01f
manavi-amanda <manaviamandahettiarachchi@gmail.com>
2026-05-29 09:28:56 +0530

    Deployment debug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict this
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
